d4da845e61d6790a0aff8501aa868c6c2d406759
AnnaLipetska <a.lipetska@gmail.com>
2020-05-08 12:11:52 +0300

    Maven installed

--- src/main/java/lesson5/app/Server.java
+++ src/main/java/lesson5/app/Server.java
@@ -1,14 +1,19 @@
 package main.java.lesson5.app;
 
-public class Server {
-//    private String[] arr = {"Vova", "Andrey", "Alexander"};
-//
-//    public String getusers() {
-//        String result;
-//        for (int i = 0; i < this.arr.length; i++) {
-//            result += this.arr[i] + "";
-//        }
-    //Escape Sequence	Description
-    //\b	Insert a backspace in the text at this point.
+public class Server { // Будет выполнять очень примитивную логику
+    // Допустим у нас будет некая база данных
+    private String[] arr = {"Vova", "Andrey", "Alexandr"}; // База данных пользователей
+
+    // Метод который будет возовращать список пользователей
+    // в виде одной строки, разделенных через запятую:
+    public String getUsers() {
+        String result = "";
+        for (int i = 0; i < this.arr.length; i++) {
+            //result += this.arr[i] + ", "; // в таком случае запятая будет и после последнего значения
+            result += this.arr[i] + (i < this.arr.length - 1 ? ", " : "");
+        }
+        return result;
+    }
+
 }
 

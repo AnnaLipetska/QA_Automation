2e14255a7ca93776a50d12792162ca17c6857d75
AnnaLipetska <a.lipetska@gmail.com>
2020-05-31 17:00:09 +0300

    Tests failing

--- src/test/java/TestBaseSetup.java
+++ src/test/java/TestBaseSetup.java
@@ -25,7 +25,7 @@ public class TestBaseSetup {
         options.addArguments("--window-size=1300,1080");
         // driver = new ChromeDriver();
         try {
-            driver = new RemoteWebDriver(new URL("http://127.0.0.1:4444/wd/hub"), options);
+            driver = new RemoteWebDriver(new URL("http://http://ec2-3-17-186-224.us-east-2.compute.amazonaws.com:4444//wd/hub"), options);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }

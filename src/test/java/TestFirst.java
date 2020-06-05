c545d47c8dea917758e0d6140a80bee99a57c5d9
AnnaLipetska <a.lipetska@gmail.com>
2020-06-05 14:43:18 +0300

    Random.java added. Some tests added to TestFirst.java

--- src/test/java/TestFirst.java
+++ src/test/java/TestFirst.java
@@ -1,18 +1,26 @@
 package test.java;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import test.java.po.ContactPage;
+import test.java.po.FaqPage;
 import test.java.po.HomePage;
 import test.java.utils.RetryAnalyzer;
 
+import static org.testng.Assert.assertTrue;
 import static org.testng.Assert.fail;
 
 public class TestFirst extends TestBaseSetup {
     HomePage homePage;
+    ContactPage contactPage;
+    FaqPage faqPage;
 
     @BeforeMethod
     public void initialize() {
         homePage = new HomePage(driver);
+        contactPage = new ContactPage(driver);
+        faqPage = new FaqPage(driver);
     }
 
 /*    @Test
@@ -22,7 +30,33 @@ public class TestFirst extends TestBaseSetup {
 
     @Test()
     public void testB() {
-        homePage.open();
-        fail();
+        homePage.open().clickContacts();
+        contactPage.clickFaq();
+        faqPage.getQuestions();
+    }
+
+    @Test()
+    public void testC() {
+        homePage.open().clickContacts();
+        contactPage.clickFaq();
+        faqPage.getQuestions();
+    }
+
+    @Test()
+    public void testD() {
+        homePage.open().clickContacts();
+        contactPage.clickFaq();
+        faqPage.getQuestions();
+        assertTrue(false);
+    }
+
+    @Test()
+    public void testE() {
+        homePage.open().clickContacts();
+        //throw new Error("Error");
+        driver.findElement(By.id("AAAA")).click();
+        contactPage.clickFaq();
+        faqPage.getQuestions();
+        assertTrue(true);
     }
 }
